ad3280b587978ba2b25d550e8e0e53a24cad6de2
kadaltidur <164900644+kadaltidur@users.noreply.github.com>
2024-03-25 22:08:02 +0700

    Add files via upload

---
+++ Student_Biodata_Website-main/assets/js/main.js
@@ -0,0 +1,32 @@
+// alert("Welcome Folks!!!");
+
+/*Moving the Nav bar -- will use this later in other pages*/
+
+// let nav = document.querySelector(".nav");
+// window.onscroll = ()=>{
+//     if(document.documentElement.scrollTop >= 100)
+//     {
+//         nav.style.top = "0px";
+//         nav.style.position = "sticky";
+//         nav.style.transition = "0.4s ease-in"
+//     }
+//     else{
+//         nav.style.top = "-70px";
+//         nav.style.transition = "0.4s ease-out"
+//         nav.style.position = "absolute";
+//     }
+// }
+
+// let typed = document.getElementById("typewriter");
+// let getText= typed.innerHTML;
+// typed.innerHTML = "";
+// let i = 0;
+// function typewriter(){
+//     if(i<getText.length)
+//     {
+//         typed.innerHTML += getText.charAt(i);
+//         i++;
+//         setTimeout(typewriter, 20);
+//     }
+// }
+// typewriter();
